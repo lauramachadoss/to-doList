e872de1da2e60ed96e11f4b0b71dd966255ee2d2
Laura Machado Antunes Santos <lauramachadosscontato@gmail.com>
2026-04-29 15:28:10 -0300

    inicia back-end(javascript)

---
+++ program.js
@@ -0,0 +1,5 @@
+const inputTarefa = document.getElementById("inputTarefa");
+const inputData = document.getElementById("inputData");
+const inputHora = document.getElementById("inputHora");
+const botaoAdicionar = document.getElementById("botaoAdicionar");
+const listaTarefas = document.getElementById("listaTarefas");
