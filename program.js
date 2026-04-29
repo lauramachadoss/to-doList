1b3b18945734864e7f1de4326513855e778583b0
Laura Machado Antunes Santos <lauramachadosscontato@gmail.com>
2026-04-29 16:33:00 -0300

    add: botao remover + evento do botaoAdicionar

--- program.js
+++ program.js
@@ -3,3 +3,14 @@ const inputData = document.getElementById("inputData");
 const inputHora = document.getElementById("inputHora");
 const botaoAdicionar = document.getElementById("botaoAdicionar");
 const listaTarefas = document.getElementById("listaTarefas");
+
+botaoAdicionar.addEventListener("click", function () {
+    const texto = inputTarefa.value;
+    const data = inputData.value;
+    const hora = inputHora.value;
+
+    if (texto === "") {
+        alert("Por favor, insira uma tarefa!");
+        return;
+    }
+})
